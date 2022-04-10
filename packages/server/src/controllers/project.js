a97b60f600e7a128dee9cf291be08a452dc1d560
Yakima <cleveryun@163.com>
2022-04-10 14:45:51 +0800

    添加项目伪接口

---
+++ packages/server/src/controllers/project.js
@@ -0,0 +1,50 @@
+const createProject = (ctx) => {
+    ctx.body = {
+        code: '200',
+        message: '操作成功',
+    }
+}
+
+const retrieveProjects = (ctx) => {
+    ctx.body = {
+        code: '200',
+        message: '操作成功',
+    }
+}
+
+const retrieveProject = (ctx) => {
+    ctx.body = {
+        code: '200',
+        message: '操作成功',
+    }
+}
+
+const updateProject = (ctx) => {
+    ctx.body = {
+        code: '200',
+        message: '操作成功',
+    }
+}
+
+const deleteProject = (ctx) => {
+    ctx.body = {
+        code: '200',
+        message: '操作成功',
+    }
+}
+
+const deleteUserFromProject = (ctx) => {
+    ctx.body = {
+        code: '200',
+        message: '操作成功',
+    }
+}
+
+module.exports = {
+    createProject,
+    retrieveProjects,
+    retrieveProject,
+    updateProject,
+    deleteProject,
+    deleteUserFromProject,
+}
--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -3,10 +3,14 @@ const Router = require('@koa/router');
 
 const companyController = require('./controllers/company.js')
 const userController = require('./controllers/user.js')
+const projectController = require('./controllers/project.js')
 
 const app = new Koa();
 const router = new Router();
 
+/**
+ * 项目比较小，所有接口都直接列到这里，一目了然
+ */
 // 公司相关接口
 router.post('/companies', companyController.createCompany)
 router.get('/companies/:companyId', companyController.retrieveCompany)
@@ -15,6 +19,14 @@ router.get('/companies/:companyId', companyController.retrieveCompany)
 router.post('/users', userController.createUser)
 router.get('/users', userController.retrieveUsers)
 router.get('/users/:userId', userController.retrieveUser)
+
+// 项目相关接口
+router.post('/projects', projectController.createProject)
+router.get('/projects', projectController.retrieveProjects)
+router.get('/projects/:projectId', projectController.retrieveProject)
+router.put('/projects/:projectId', projectController.updateProject)
+router.delete('/projects/:projectId', projectController.deleteProject)
+router.delete('/projects/deleteUserFromProject', projectController.deleteUserFromProject)
 
 app
     .use(router.routes())
